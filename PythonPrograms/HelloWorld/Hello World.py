7dfd017197dda0291b0285458dfd09d32c386f06
Myka Martinez <michaelmartinez-mann@Yin.attlocal.net>
2018-02-28 17:38:37 -0600

    Completed 'Understanding More About Python'

--- PythonPrograms/HelloWorld/Hello World.py
+++ PythonPrograms/HelloWorld/Hello World.py
@@ -1,14 +1,41 @@
-print('Hello, World!')
-print(1+2)
-print(7*6)
-print()
-print("The End")
-print("Python's strings are easy to use")
-print('We can even include "quotes" in strings')
-print("hello" + " world")
+#print('Hello, World!')
+#print(1+2)
+#print(7*6)
+#print()
+#print("The End")
+#print("Python's strings are easy to use")
+#print('We can even include "quotes" in strings')
+#print("hello" + " world")
 
-greeting = "hello"
-name = "bruce"
-print(greeting + name)
+#greeting = "hello"
+#name = "bruce"
+#print(greeting + name)
 #If we want a space we can add that too
-print(greeting + ' ' + name)
+#print(greeting + ' ' + name)
+
+
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
+
+# splitString = "This has been\nsplit over\nseveral\nline."
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t"
+# print(tabbedString)
+
+print('The pet shop owner said "No, no \'e\'s uh,...he\'s resting"')
+print("The pet shop owner said \"No, no 'e's uh,...he's resting\"")
+
+anotherSplitString = """This string had been
+split over
+several lines"""
+print(anotherSplitString)
+
+yetAnotherSplitString = """Testing 1\
+2\
+3"""
+print(yetAnotherSplitString)
+
+print('''The pet shop owner said "No, no 'e's uh,...he's resting" ''')
+print("""The pet shop owner said "No, no 'e's uh,...he's resting" """)
