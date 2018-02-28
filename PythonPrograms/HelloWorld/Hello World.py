8bf2f18938bff11f8642908c24dc3dc865a7ec9b
Myka Martinez <michaelmartinez-mann@Yin.attlocal.net>
2018-02-28 17:17:28 -0600

    'Getting to Know Python' completed

--- PythonPrograms/HelloWorld/Hello World.py
+++ PythonPrograms/HelloWorld/Hello World.py
@@ -3,3 +3,12 @@ print(1+2)
 print(7*6)
 print()
 print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+
+greeting = "hello"
+name = "bruce"
+print(greeting + name)
+#If we want a space we can add that too
+print(greeting + ' ' + name)
