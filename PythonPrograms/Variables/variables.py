12f38f0009543ea15ddd8951530d0e15120aad5a
Myka Martinez <michaelmartinez-mann@Yin.attlocal.net>
2018-02-28 18:02:28 -0600

    Completed 'Storing Items in Variables, Part A'

---
+++ PythonPrograms/Variables/variables.py
@@ -0,0 +1,11 @@
+greeting = "Bruce"
+_myName = "Myka"
+Myka37 = "Good"
+Myka_Was_36 = "Hello"
+Greeting = "There"
+print(Myka_Was_36 + ' ' + greeting)
+
+age = 24
+print(age)
+
+print(greeting + age)
