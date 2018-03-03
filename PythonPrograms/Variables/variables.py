8d65bc68621e94817ee6674c7a39b10c6abab1ab
Myka Martinez <michaelmartinez-mann@Yin.local.tld>
2018-03-03 17:43:53 -0600

    Completed 'Storing Items in Variables, Part B'

--- PythonPrograms/Variables/variables.py
+++ PythonPrograms/Variables/variables.py
@@ -1,11 +1,34 @@
-greeting = "Bruce"
-_myName = "Myka"
-Myka37 = "Good"
-Myka_Was_36 = "Hello"
-Greeting = "There"
-print(Myka_Was_36 + ' ' + greeting)
+# greeting = "Bruce"
+# _myName = "Myka"
+# Myka37 = "Good"
+# Myka_Was_36 = "Hello"
+# Greeting = "There"
+# print(Myka_Was_36 + ' ' + greeting)
+#
+# age = 24
+# print(age)
+#
+# print(greeting + age)
 
-age = 24
-print(age)
 
-print(greeting + age)
+# a = 12
+# b = 3
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+# for i in range(1, a//b):
+#     print(i)
+
+a = 12
+b = 3
+
+print(a + b / 3 - 4 * 12)
+print(8 / 2 *3)
+print(8 * 3 / 2)
+
+print((((a+b) / 3) - 4) * 12)
