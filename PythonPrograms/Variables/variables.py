51677aab6e01aaf28534ee77766364c2c0be8b2d
Myka Martinez <michaelmartinez-mann@Yin.attlocal.net>
2018-03-08 11:41:28 -0600

    Completed 'Variables and Strings'

--- PythonPrograms/Variables/variables.py
+++ PythonPrograms/Variables/variables.py
@@ -23,12 +23,53 @@
 #
 # for i in range(1, a//b):
 #     print(i)
+#
+# a = 12
+# b = 3
+#
+# print(a + b / 3 - 4 * 12)
+# print(8 / 2 *3)
+# print(8 * 3 / 2)
+#
+# print((((a+b) / 3) - 4) * 12)
+#
+# c = a + b
+# d = c / 3
+# e = d - 4
+# print(e * 12)
 
-a = 12
-b = 3
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+print(parrot[0])
+print(parrot[-1])
+print(parrot[0:6])
+#              ^ Up to but not including position 6
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
 
-print(a + b / 3 - 4 * 12)
-print(8 / 2 *3)
-print(8 * 3 / 2)
+number = "9,223,372,036,854,775,807"
+print(number[1::4] )
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
 
-print((((a+b) / 3) - 4) * 12)
+string1 = "he's "
+string2 = "probably "
+string3 = "pining"
+
+print(string1 + string2 + string3)
+print("he's " "probably " "pining")
+print("he's probably pining")
+
+print("Hello " * 5)
+print("Hello " * (5 +4))
+print("Hello " * 5 + "4")
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "fjord")
