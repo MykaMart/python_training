11f3fba2d11c467c3d6d5144023ab63b91c6a387
Myka Martinez <michaelmartinez-mann@Yin.attlocal.net>
2018-03-08 14:44:24 -0600

    Completed 'Tabs'

---
+++ PythonPrograms/Tabs/tabs.py
@@ -0,0 +1,13 @@
+for i in range(1, 12):
+    print("No. {:2} squared is {:3} and cubed is {:4}.".format(i, i ** 2, i ** 3))
+    # print("Calculation complete.")
+    # print("Try again.")
+# ^ must have indentation to know it is part of the previous lines code, or it's "block"
+
+
+# Unlike Javascript, Python does not use delimiters such as {
+
+# for (let i = 1; i <= 12; i++){
+#     console.log("No. " + i.tostring() + "squared is" + (i*i).tostring() + " and cubed is " + (i*i*i).tostring()
+#
+# }
